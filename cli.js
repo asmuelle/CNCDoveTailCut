66d395636c80d8b45ac1523af7272f03a9cfde94
Andreas Müller <herban.mueller@gmail.com>
2016-01-26 16:11:23 +0100

    Update cli.js

--- cli.js
+++ cli.js
@@ -4,14 +4,19 @@
 var cncdovetailcut = require('./lib/cncdovetailcut');
 
 var userArgs = process.argv;
-var searchParam = userArgs[2];
+var searchParam = userArgs[6];
 
 var commandline = require('commander');
 
 commandline
   .command('gcode')
   .description('create gcode')
-  .option("-d, --toolDiameter [mm]", "Tool Diameter")
+  .option("-d, --toolDiameter [cm]", "Tool Diameter")
+  .option("-x, --width [cm]", "Cutting width")
+  .option("-y, --height [cm]", "Cutting Width")
+  .option("-z, --depth [cm]", "Cutting Depth")
+  .option("-s, --depthOfCut [cm]", "Depth of Cut")
+  .option("-a, --alignment [inside|outside|center]", "Alignment of Cut")
   
   .action(function(options){
     if(options.toolDiameter ){
